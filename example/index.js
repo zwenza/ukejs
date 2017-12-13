23aa9c64a6e8e390caa318cd7a6f63319feb408d
David Jöch <david.joech@gmail.com>
2017-12-13 22:12:46 +0100

    JSX!

--- example/index.js
+++ example/index.js
@@ -4,23 +4,17 @@ var router = Router();
 const HelloComponent = new Uke({
     selector: 'hello',
     render: function () {
-        return u('div', null, [
-            u('h1', null, [
-                'Hello World!!!'
-            ]),
-            u('h3', null, [
-                'Virtual DOM works!'
-            ])
-        ])
+        return <div>
+            <h1>Hello</h1>
+            <h2>JSX World!</h2>
+        </div>
     }
 });
 
 const WorldComponent = new Uke({
     selector: 'world',
     render: function () {
-        return u('h2', null, [
-            'You pressed the button!'
-        ])
+        return <h2>You pressed it!</h2>
     }
 });
 
