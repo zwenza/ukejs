676b24defb021cd9f3d59eea00f4b9fe51a2ab90
David Jöch <david.joech@gmail.com>
2017-12-13 21:35:32 +0100

    implemented VDOM diffing

--- index.js
+++ index.js
@@ -15,7 +15,21 @@ const HelloComponent = new Uke({
     }
 });
 
+const WorldComponent = new Uke({
+    selector: 'world',
+    render: function () {
+        return u('h2', null, [
+            'You pressed the button!'
+        ])
+    }
+});
+
 router.route('/', HelloComponent);
 
 app.router(router);
 app.mount('app');
+
+const btn = document.getElementById('btn1');
+btn.addEventListener('click', function() {
+    updateElement(document.getElementById('app'), WorldComponent.render(), HelloComponent.render());
+});
